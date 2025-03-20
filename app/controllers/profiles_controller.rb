0561efba110496b0b63bf97dd2a7ba1ec553c904
beatrizbraga <biabbraga@gmail.com>
2025-03-20 18:41:55 -0300

    ranking back

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,11 @@
 class ProfilesController < ApplicationController
 
   def ranking
-    @users = User.all
+    @users = User
+    .left_joins(:reviews)                                   # Join users with their reviews
+    .group('users.id')                                      # Group by user to get review counts
+    .select('users.*, COUNT(reviews.id) AS reviews_count')  # Select users and count their reviews
+    .order('reviews_count DESC')
   end
 
 end
