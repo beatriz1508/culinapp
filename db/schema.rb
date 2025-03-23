0b9d9909686a8831b61500e5fd89912ca3286fe8
Maria Eduarda Montenegro <memtmontenegro@gmail.com>
2025-03-23 19:26:19 -0300

    finalizado

--- db/schema.rb
+++ db/schema.rb
@@ -42,15 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_23_212312) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "chatbot_questions", force: :cascade do |t|
-    t.text "user_question"
-    t.text "ai_answer"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_chatbot_questions_on_user_id"
-  end
-
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.string "quantity"
@@ -296,7 +287,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_23_212312) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "chatbot_questions", "users"
   add_foreign_key "ingredients", "tasks"
   add_foreign_key "journeys", "paths"
   add_foreign_key "journeys", "users"
