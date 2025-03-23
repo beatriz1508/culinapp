338e05aa273981d398ff16831544b7d7e5ed4f55
kaue ogibowski <kaueogibowski@outlook.com>
2025-03-23 16:49:50 -0300

    sound added

---
+++ app/javascript/controllers/sound_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["audio"]
+
+  connect() {
+    // Este método é chamado quando o controller é conectado ao DOM
+    console.log("Olá mundo")
+  }
+
+  play() {
+    console.log("Olá mundo")
+    this.audioTarget.play()
+  }
+}
