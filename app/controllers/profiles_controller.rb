7445a067b3fac17ee25fe872f9fba7a090da11c6
beatrizbraga <biabbraga@gmail.com>
2025-03-23 21:25:25 -0300

    new-rankings

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,12 +2,21 @@
 class ProfilesController < ApplicationController
 
   def ranking
+    # Recipes Ranking (based on reviews)
     @users = User
     .left_joins(:reviews)                                   # Join users with their reviews
     .group('users.id')                                      # Group by user to get review counts
     .select('users.*, COUNT(reviews.id) AS reviews_count')  # Select users and count their reviews
     .order('reviews_count DESC')
-  end
+
+     # Badges Ranking (based on badge logic)
+    @users_badges_ranking = User.all.sort_by do |user|
+    user.reviews.joins(task: :world).group("worlds.id").count.select { |_world_id, review_count| review_count == 5 }.size
+    end.reverse
+
+    @current_user_recipe_rank = @users.index(current_user) + 1
+    @current_user_badge_rank = @users_badges_ranking.index(current_user) + 1
+   end
 
   def show
     @badges = current_user.reviews.joins(task: :world).group("worlds.id").count
@@ -24,6 +33,5 @@ class ProfilesController < ApplicationController
       redirect_to user_root_path, alert: "Erro ao enviar foto."
     end
   end
-
 
 end
